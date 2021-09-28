1ae274a25b0b87175244fcd6f8391cd23a26fa59
sidartta <mohamed-amine.khalf@hec.ca>
2021-09-28 17:21:12 -0400

    files ready for review

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  principal = document.getElementById('principal').value
+  rate = document.getElementById('rate').value
+  years = document.getElementById('years').value
+
+  futureValue = (principal * (1 + rate / 100)) ^ years
+  todayYear = new Date().getFullYear()
+  futureYear = todayYear + parseInt(years)
+
+  let resultCapital = document.getElementById('result-capital')
+  let resultRate = document.getElementById('result-rate')
+  let resultFutureValue = document.getElementById('result-future-value')
+  let resultFutureYear = document.getElementById('result-future-year')
+
+  resultCapital.innerText = principal
+  resultRate.innerText = rate + '%'
+  resultFutureValue.innerText = futureValue
+  resultFutureYear.innerText = futureYear
+
+  resultCapital.style.backgroundColor = 'yellow'
+  resultRate.style.backgroundColor = 'yellow'
+  resultFutureValue.style.backgroundColor = 'yellow'
+  resultFutureYear.style.backgroundColor = 'yellow'
+
+  let targetDiv = document.getElementById('result')
+
+  targetDiv.style.visibility = 'visible'
 }
-        
